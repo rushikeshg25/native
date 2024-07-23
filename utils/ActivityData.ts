58877fa23e4fc2ccf3b1685bc452897ae42f9d6f
Amit Dawande <amitdawande333@gmail.com>
2024-07-23 17:33:14 +0530

    activity tab commits

---
+++ utils/ActivityData.ts
@@ -0,0 +1,23 @@
+export const ActivityData = [
+  {
+    id: '1',
+    activity: 'You paid Rs.2000 to John',
+    image: 'https://picsum.photos/200',
+    summary: 'You owe $20',
+    date: 'Yesterday,8:30pm',
+  },
+  {
+    id: '2',
+    activity: 'You are owned Rs.550 from Flatmate',
+    image: 'https://picsum.photos/200',
+    summary: 'You get back $55',
+    date: '6 days ago,7:00am',
+  },
+  {
+    id: '3',
+    activity: 'You recorded a payment from Kong',
+    image: 'https://picsum.photos/200',
+    summary: 'You received $60',
+    date: 'jul 10 9:30pm',
+  },
+];
